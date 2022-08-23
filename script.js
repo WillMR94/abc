f4f11913723048d2d6c5f289ec333c872fdb23a0
WillMR94 <108240544+WillMR94@users.noreply.github.com>
2022-08-23 20:21:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,202 @@
+  const cardAa = document.querySelector('.aa')
+  const cardBb = document.querySelector('.bb')
+  const cardCc = document.querySelector('.cc')
+  const cardDd = document.querySelector('.dd')
+  const cardEe = document.querySelector('.ee')
+  const cardFf = document.querySelector('.ff')
+  const cardGg = document.querySelector('.gg')
+  const cardHh = document.querySelector('.hh')
+  const cardIi = document.querySelector('.ii')
+  const cardJj = document.querySelector('.jj')
+  const cardLl = document.querySelector('.ll')
+  const cardMm = document.querySelector('.mm')
+  const cardNn = document.querySelector('.nn')
+  const cardOo = document.querySelector('.oo')
+  const cardPp = document.querySelector('.pp')
+  const cardQq = document.querySelector('.qq')
+  const cardRr = document.querySelector('.rr')
+  const cardSs = document.querySelector('.ss')
+  const cardTt = document.querySelector('.tt')
+  const cardUu = document.querySelector('.uu')
+  const cardVv = document.querySelector('.vv')
+  const cardXx = document.querySelector('.xx')
+  const cardZz = document.querySelector('.zz')
+  const ButtonA = document.querySelector('#a')
+  const ButtonB = document.querySelector('#b')
+  const ButtonC = document.querySelector('#c')
+  const ButtonD = document.querySelector('#d')
+  const ButtonE = document.querySelector('#e')
+  const ButtonF = document.querySelector('#f')
+  const ButtonG = document.querySelector('#g')
+  const ButtonH = document.querySelector('#h')
+  const ButtonI = document.querySelector('#i')
+  const ButtonJ = document.querySelector('#j')
+  const ButtonL = document.querySelector('#l')
+  const ButtonM = document.querySelector('#m')
+  const ButtonN = document.querySelector('#n')
+  const ButtonO = document.querySelector('#o')
+  const ButtonP = document.querySelector('#p')
+  const ButtonQ = document.querySelector('#q')
+  const ButtonR = document.querySelector('#r')
+  const ButtonS = document.querySelector('#s')
+  const ButtonT = document.querySelector('#t')
+  const ButtonU = document.querySelector('#u')
+  const ButtonV = document.querySelector('#v')
+  const ButtonX = document.querySelector('#x')
+  const ButtonZ = document.querySelector('#z')
+  const ButtonAa = document.querySelector('#aa')
+  const SoundA = new Audio ("https://github.com/WillMR94/abcSound/blob/main/a.m4a?raw=true")
+  const SoundB = new Audio ("https://github.com/WillMR94/abcSound/blob/main/b.m4a?raw=true")
+  const SoundC = new Audio ("https://github.com/WillMR94/abcSound/blob/main/c.m4a?raw=true")
+  const SoundD = new Audio ("https://github.com/WillMR94/abcSound/blob/main/d.m4a?raw=true")
+  const SoundE = new Audio ("https://github.com/WillMR94/abcSound/blob/main/e.m4a?raw=true")
+  const SoundF = new Audio ("https://github.com/WillMR94/abcSound/blob/main/f.m4a?raw=true")
+  const SoundG = new Audio ("https://github.com/WillMR94/abcSound/blob/main/g.m4a?raw=true")
+  const SoundH = new Audio ("https://github.com/WillMR94/abcSound/blob/main/h.m4a?raw=true")
+  const SoundI = new Audio ("https://github.com/WillMR94/abcSound/blob/main/i.m4a?raw=true")
+
+  function hideCards(){
+  cardAa.classList.add('hide')
+  cardBb.classList.add('hide')
+  cardCc.classList.add('hide')
+  cardDd.classList.add('hide')
+  cardEe.classList.add('hide')
+  cardFf.classList.add('hide')
+  cardGg.classList.add('hide')
+  cardHh.classList.add('hide')
+  cardIi.classList.add('hide')
+  cardJj.classList.add('hide')
+  cardLl.classList.add('hide')
+  cardMm.classList.add('hide')
+  cardNn.classList.add('hide')
+  cardOo.classList.add('hide')
+  cardPp.classList.add('hide')
+  cardQq.classList.add('hide')
+  cardRr.classList.add('hide')
+  cardSs.classList.add('hide')
+  cardTt.classList.add('hide')
+  cardUu.classList.add('hide')
+  cardVv.classList.add('hide')
+  cardXx.classList.add('hide')
+  cardZz.classList.add('hide')
+
+  }
+
+  ButtonA.addEventListener('click',function(){
+    hideCards()
+    cardAa.classList.remove('hide')
+    SoundA.play()
+  })
+  ButtonB.addEventListener('click',function(){
+    hideCards()
+    cardBb.classList.remove('hide')
+    SoundB.play()
+  })
+  ButtonC.addEventListener('click',function(){
+    hideCards()
+    cardCc.classList.remove('hide')
+    SoundC.play()
+  })
+  ButtonD.addEventListener('click',function(){
+    hideCards()
+    cardDd.classList.remove('hide')
+    SoundD.play()
+  })
+  ButtonE.addEventListener('click',function(){
+    hideCards()
+    cardEe.classList.remove('hide')
+    SoundE.play()
+  })
+  ButtonF.addEventListener('click',function(){
+    hideCards()
+    cardFf.classList.remove('hide')
+    SoundF.play()
+  })
+  ButtonG.addEventListener('click',function(){
+    hideCards()
+    cardGg.classList.remove('hide')
+    SoundG.play()
+  })
+  ButtonH.addEventListener('click',function(){
+    hideCards()
+    cardHh.classList.remove('hide')
+    SoundH.play()
+  })
+  ButtonI.addEventListener('click',function(){
+    hideCards()
+    cardIi.classList.remove('hide')
+    SoundI.play()
+  })
+  ButtonJ.addEventListener('click',function(){
+    hideCards()
+    cardJj.classList.remove('hide')
+
+  })
+  ButtonL.addEventListener('click',function(){
+    hideCards()
+    cardLl.classList.remove('hide')
+
+  })
+  ButtonM.addEventListener('click',function(){
+    hideCards()
+    cardMm.classList.remove('hide')
+
+  })
+  ButtonN.addEventListener('click',function(){
+    hideCards()
+    cardNn.classList.remove('hide')
+
+  })
+  ButtonO.addEventListener('click',function(){
+    hideCards()
+    cardOo.classList.remove('hide')
+
+  })
+  ButtonP.addEventListener('click',function(){
+    hideCards()
+    cardPp.classList.remove('hide')
+
+  })
+  ButtonQ.addEventListener('click',function(){
+    hideCards()
+    cardQq.classList.remove('hide')
+
+  })
+  ButtonR.addEventListener('click',function(){
+    hideCards()
+    cardRr.classList.remove('hide')
+
+  })
+  ButtonS.addEventListener('click',function(){
+    hideCards()
+    cardSs.classList.remove('hide')
+
+  })
+  ButtonT.addEventListener('click',function(){
+    hideCards()
+    cardTt.classList.remove('hide')
+
+  })
+  ButtonU.addEventListener('click',function(){
+    hideCards()
+    cardUu.classList.remove('hide')
+
+  })
+  ButtonV.addEventListener('click',function(){
+    hideCards()
+    cardVv.classList.remove('hide')
+
+  })
+  ButtonX.addEventListener('click',function(){
+    hideCards()
+    cardXx.classList.remove('hide')
+
+  })
+  ButtonZ.addEventListener('click',function(){
+    hideCards()
+    cardZz.classList.remove('hide')
+
+  })
+
+
+
